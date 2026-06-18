2eeb4ff5e9499056e7aec64ba95394a38778e335
hariprasatham <hariprasatham@gmail.com>
2026-06-18 12:31:08 +0530

    initial commit

---
+++ backend/test/task.test.js
@@ -0,0 +1,22 @@
+const request = require("supertest")
+const {app, server} = require("../index")
+const mongoose = require("mongoose")
+
+describe("GET api/tasks", ()=>{
+    it("it should return 200 ok", async()=>{
+        const response = await request(app).get("/api/tasks")
+        expect(response.statusCode).toBe(200)
+    })
+
+     it("response data should array", async()=>{
+        const response = await request(app).get("/api/tasks")
+        expect(typeof response.body == "object").toBe(true)
+        expect(response.body).toHaveProperty("tasks")
+    })
+})
+
+
+afterAll(async()=>{
+    await mongoose.connection.close()
+    await server.close();
+})
